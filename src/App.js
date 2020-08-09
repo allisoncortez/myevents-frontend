eb3a35bef13b18b9dddfc2debde866dc02eb13e1
allisoncortez <allizetorc@gmail.com>
2020-08-09 12:42:31 -0500

    index.js setup, created store

--- src/App.js
+++ src/App.js
@@ -2,13 +2,6 @@ import React from 'react';
 
 class App extends React.Component {
 
-  // testing fetch
-  componentDidMount(){
-    fetch('http://localhost:3000/api/v1/events')
-      .then(response => response.json())
-      .then(data => console.log(data))
-  }
-
   render(){
     return (
       <div className="App">
--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {createStore, applyMiddleware, compose} from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+import App from './App'
 
 
-import App from './App';
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
-);
+)
+
+
+
+
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
