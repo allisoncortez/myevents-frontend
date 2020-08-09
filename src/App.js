82a6db7ecedf2d30e960e1cef066974e4efe616c
allisoncortez <allizetorc@gmail.com>
2020-08-09 12:13:55 -0500

    testing fetch data in App.js, redux dependencies added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from 'react';
 
 class App extends React.Component {
+
+  // testing fetch
+  componentDidMount(){
+    fetch('http://localhost:3000/api/v1/events')
+      .then(response => response.json())
+      .then(data => console.log(data))
+  }
+
   render(){
     return (
       <div className="App">
