588b9095961d12a40c21abe6894c50baa5e8ef89
allisoncortez <allizetorc@gmail.com>
2020-08-13 15:54:20 -0500

    comment form set up, submit not working yet

--- src/components/CommentInput.js
+++ src/components/CommentInput.js
@@ -1,11 +1,38 @@
 import React from 'react'
+import {connect} from 'react-redux'
 
 class CommentInput extends React.Component {
+
+    state = {
+        description:''
+    }
+
+    handleOnChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleOnSubmit = (e) => {
+        e.preventDefault()
+
+    }
     render() {
         return (
-            <div>comment input</div>
+            <div>
+                <form>
+                    <textarea rows="5" cols="33"
+                    placeholder="what did you think?"
+                    name="description"
+                    value={this.state.description}
+                    onChange={this.handleOnChange}/>
+                    <br></br>
+                    <input type="submit" />
+                </form>
+                <br></br>
+            </div>
         )
     }
 }
 
-export default CommentInput
+export default connect(null)(CommentInput)
