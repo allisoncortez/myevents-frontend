c23611e055d85aa32a900065a166af45c9938106
allisoncortez <allizetorc@gmail.com>
2020-08-13 14:42:01 -0500

    successfully iterated through comments props in the Comments component

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,8 +1,15 @@
 import React from 'react'
 
 const Comments = (props) => {
+    console.log(props.comments)
+
+
     return (
-        <div>Comments</div>
+        <div>
+            {props.comments && props.comments.map(comment =>
+                <li key={comment.id}>{comment.description}</li>
+            )}
+        </div>
     )
 }
 
