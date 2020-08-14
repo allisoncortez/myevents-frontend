994f8a64594390771fe9d046603bb486c4e5212e
allisoncortez <allizetorc@gmail.com>
2020-08-14 14:33:30 -0500

    NavBar component added into App.js, initial file setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import EventsContainer from './containers/EventsContainer'
+import NavBar from './components/NavBar'
 
 const App = () => {
     return (
       <div className="App">
+        <NavBar />
         <EventsContainer />
       </div>
     )
