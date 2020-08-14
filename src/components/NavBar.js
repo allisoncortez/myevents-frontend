e47292cad881ae8b3266111318cd01e1dac099f6
allisoncortez <allizetorc@gmail.com>
2020-08-14 15:28:14 -0500

    links added to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const NavBar = () => {
     return (
-        <div>navbar</div>
+        <div class="ui borderless huge menu">
+            <a class="item">
+            <Link to="/">AAMO</Link>
+            </a>
+            <a class="item">
+                <Link to="/events">Browse</Link>
+            </a>
+            <div class="right menu">
+                <a class="item">
+                <Link to="/events/new">add an EVENT</Link>
+                </a>
+            </div>
+        </div>
     )
 }
 
