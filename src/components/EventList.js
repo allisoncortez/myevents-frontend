eb4d6844892c9b3f71fc8695cb38d91b86dcb81a
allisoncortez <allizetorc@gmail.com>
2020-08-14 17:43:44 -0500

    updates to EventList styling

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -5,11 +5,13 @@ class EventList extends Component {
     renderEvents() {
         return this.props.events.map((event) => {
             return (
-                <div className="item" key={event.id}>
-                        <div className="content">
-                            <h1><Link to={`/events/${event.id}`}> {event.attributes.title} @ 7 p.m.</Link></h1>
-                            {/* <div className="description"> <h3>{event.attributes.description}</h3></div> */}
-                        </div>
+                <div className="ui basic center aligned segment">
+                    <div className="item" key={event.id}>
+                            <div className="content">
+                                <h1><Link to={`/events/${event.id}`}> {event.attributes.title} @ 7 p.m.</Link></h1>
+                                {/* <div className="description"> <h3>{event.attributes.description}</h3></div> */}
+                            </div>
+                    </div>
                 </div>
             )
         })
