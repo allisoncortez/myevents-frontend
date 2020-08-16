237daae346cda75fb177fae469081ebb2709a682
allisoncortez <allizetorc@gmail.com>
2020-08-16 15:59:51 -0500

    EventList styled and centered

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -2,33 +2,32 @@ import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
 import Header from './Header'
 
-
 class EventList extends Component {
     renderEvents() {
         return this.props.events.map((event) => {
             return (
-                <div className="ui celled list">
-                    <div className="item" key={event.id}>
-                            <div className="content">
-                                <Link to={`/events/${event.id}`}> <h1 className="ui black header huge">{event.attributes.title} @ 7 p.m. </h1></Link>
-                                {/* <div className="description"> <h3>{event.attributes.description}</h3></div> */}
-                            </div>
+                <div>
+                    <div key={event.id}>
+                        <h5 className="ui header centered">HOSTNAME</h5>
+                        <Link to={`/events/${event.id}`}><h1 className="ui header centered huge">{event.attributes.title}</h1></Link>
+                        <h5 className="ui header centered">{event.attributes.location} @ 7 p.m.</h5>
+                        <div className="ui clearing divider"></div>
                     </div>
+                    <br></br>
+                    
                 </div>
             )
         })
-
     } 
     
     render(){
         return (   
             <div>
                 <Header />
-                <div ui container>
-                <div className="ui basic center aligned segment">
+                <br></br>
+                <div className="ui container">
                     {this.renderEvents()}
                 </div>
-            </div>
             </div>
         )
     }
