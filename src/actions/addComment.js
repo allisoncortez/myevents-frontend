c08aa966563ca7783d99f2bca18e0de551d7579b
allisoncortez <allizetorc@gmail.com>
2020-08-25 16:53:35 -0400

    updated fetch to return entire event, mapped through event in reducer to update comments upon submission

--- src/actions/addComment.js
+++ src/actions/addComment.js
@@ -1,3 +1,18 @@
+// export const addComment = (comment, eventId) => {
+//     return (dispatch) => {
+//         fetch(`http://localhost:3000/api/v1/events/${eventId}/comments`, {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json',
+//             },
+//             body: JSON.stringify(comment)
+//         })
+//         .then(resp => resp.json())
+//         // .then(event => console.log(event))
+//         .then(comment => dispatch({ type: 'ADD_COMMENT', payload: comment }))
+//     }
+// }
+
 export const addComment = (comment, eventId) => {
     return (dispatch) => {
         fetch(`http://localhost:3000/api/v1/events/${eventId}/comments`, {
@@ -8,7 +23,6 @@ export const addComment = (comment, eventId) => {
             body: JSON.stringify(comment)
         })
         .then(resp => resp.json())
-        // .then(event => console.log(event))
-        .then(comment => dispatch({ type: 'ADD_COMMENT', payload: comment }))
+        .then(event => dispatch({ type: 'ADD_COMMENT', payload: event.data}))
     }
 }
--- src/containers/CommentsContainer.js
+++ src/containers/CommentsContainer.js
@@ -3,9 +3,13 @@ import CommentInput from '../components/CommentInput'
 import Comments from '../components/Comments'
 
 class CommentsContainer extends React.Component {
+
+    // componentDidMount(){
+    //     this.props.fetchComments()
+    // }
     
     render() {
-        // console.log(this.props)
+        console.log(this.props)
         return (
             <div className="ui comments">
                 <div className="ui dividing header">Comments</div>
@@ -15,5 +19,11 @@ class CommentsContainer extends React.Component {
         )
     }
 }
+
+// const mapStateToProps = state => {
+//     return {
+//         comments: state.comments
+//     }
+// }
 
 export default CommentsContainer
