45812a006a81eec3cfcf567d3da5d19bf6176b65
allisoncortez <allizetorc@gmail.com>
2021-01-04 19:02:57 -0600

    fix syntax in deleteComment fetch request

--- src/actions/deleteComment.js
+++ src/actions/deleteComment.js
@@ -1,6 +1,6 @@
 export const deleteComment = (commentId, eventId) => {
     return (dispatch) => {
-        return fetch(`http://localhost:3000/api/v1/events/${eventId}/comments/${commentId}`, {
+        fetch(`http://localhost:3000/api/v1/events/${eventId}/comments/${commentId}`, {
             method: 'DELETE'
         })
         .then(response => response.json())
